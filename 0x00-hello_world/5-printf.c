c7c73e96db08c26d72937849ccd3e38806a9c14b
Blessingokafor <nomeblessing1@yahoo.com>
2022-10-14 17:09:03 -0700

    hyg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
 *main - entry point
-
 *
 *Description - with proper grammar, but the outcome is a piece of art,
 *
 *Return: always 0 (success)
 */
-
 
 int main(void)
 {
