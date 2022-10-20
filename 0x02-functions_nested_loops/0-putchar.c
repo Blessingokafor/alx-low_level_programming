5a57e6148dd43cb9cbafc4745a0565fb17e01710
Blessingokafor <nomeblessing1@yahoo.com>
2022-10-20 16:20:06 -0700

    dvd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
- * main - Prints Holberton as a message.
- *
- * Return: Always 0 (Success)
- */
+**main - Entry point
+*
+*@char : description, its a character type
+*@_putchar() : is a funcyion
+*Return: Always 0 (success)
+*
+*/
+
+char _putchar(char);
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-	return (0);
+char *ch = "_putchar";
+while (*ch)
+{
+_putchar(*ch);
+ch++;
+}
+{
+_putchar('\n');
+}
+return (0);
 }
