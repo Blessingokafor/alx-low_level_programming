49793b47cbc04a4dc03e968b535bdd7c02a88f75
Blessingokafor <nomeblessing1@yahoo.com>
2022-10-26 14:09:10 -0700

    bsb

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,113 @@
+sing Okafor(you)
+
+	Blessing Okafor
+
+	LOS-0922
+
+	This space is just for you. Jot down notes, list your to-dos, or keep links and files handy. You can also talk to yourself here, but please bear in mind that you’ll have to supply both sides of the conversation.
+
+
+
+
+
+	Blessing Okafor
+
+	  22:13
+
+	  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 (edited) 
+
+
+
+
+
+	Blessing Okafor
+
+	  18:26
+
+	  cA. Interviewee’s name Ajulukwu chidozie
+
+	  B. Time and place of interview 2:30 pm 28/09/2022
+
+	  C. List several statements made by each interviewee. Indicate next to each statement if it is an example of a growth mindset or fixed mindset. List each statement and label it either “growth mindset” or “fixed mindset”.
+
+	  Growth mindset
+
+	  1 My target is to own a big shop next year, that is why I double hustle this year to achieve the goal -growth mindset
+
+	  2 I have done all my best I think GOD has special people he reserved being great. Therefore, I am good the position I found myself. - fixed mindset
+
+	  3 I don’t understand how the economy of our is and I am to stable finance but I believe In next few months to come I will work hard to start my building projects – growth mindset
+
+	  4 my child has refused to go to school, but I won’t give up on him. growth mindset
+
+	  D. Did your interviewee’s stories involve grit (or lack of grit)? Explain.
+
+	  It involves grit because from his stories he is putting efforts to achieve his goal no matter the obstacles and challenges his stories still involve perseverance, effort and hard working
+
+	  E. What surprised you about conducting this interview? What did you learn?
+
+	  What surprised me about conducting the interview was how he was responding even when I thought everyone will ignore my request, but I found one person who  accepted to tell his stories, from his stories I learned that had work pay, resilience pay putting effort in all we do shift us from a place of nothing to a place of plenty
+
+
+
+
+
+	  Blessing Okafor
+
+	    00:38
+
+	    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
+
+
+
+
+
+	    Blessing Okafor
+
+	      03:04
+
+	      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
+
+
+
+
+
+	      Blessing Okafor
+
+	        13:22
+
+		void reset_to_98(int *n);
+
+		void swap_int(int *a, int *b);
+
+		int _strlen(char *s);
+
+		void _puts(char *str);
+
+		int _putchar(char c);
+
+		void print_rev(char *s);
+
+		void rev_string(char *s);
+
+		void puts2(char *str);
+
+		void puts_half(char *str);
+
+		void print_array(int *a, int n);
+
+		char *_strcpy(char *dest, char *src);
+
+		int _atoi(char *s);
+
+		Footer
+
+		:copyright: 2022 GitHub, Inc.
+
+		Footer navigation
+
+		Terms
+
+		Privacy
+
+		Security
