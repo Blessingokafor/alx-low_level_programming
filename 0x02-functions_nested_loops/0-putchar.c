63c062a2afa2e6f164eb338f67c46e40348995b0
Blessingokafor <nomeblessing1@yahoo.com>
2022-10-27 15:51:24 -0700

    ggh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
 /**
-**main - Entry point
-*
+*main - Entry point
 *@char : description, its a character type
 *@_putchar() : is a funcyion
 *Return: Always 0 (success)
-*
 */
-
 char _putchar(char);
 int main(void)
 {
