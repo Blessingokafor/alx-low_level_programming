8ce6707703f8b793435dca62b625d9d463b0504c
Blessingokafor <nomeblessing1@yahoo.com>
2022-10-27 13:53:02 -0700

    hfff

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,9 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-Footer
-©️ 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
